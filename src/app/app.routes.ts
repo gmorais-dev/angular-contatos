ec682537d42ba80f5a2bcf7e7a47569040c6c67b
Gmorais-dev <guilherme_morais14@hotmail.com>
2024-08-28 11:13:32 -0300

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroContatoComponent } from './pages/cadastro-contato/cadastro-contato.component';
+import { ConsultaContatoComponent } from './pages/consulta-contato/consulta-contato.component';
+import { authGuard } from './core/guards/auth.guard'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cadastro', component: CadastroContatoComponent, canActivate: [authGuard] },
+  { path: 'consulta', component: ConsultaContatoComponent },
+  { path: '', redirectTo: '/consulta', pathMatch: 'full' },
+  { path: '**', redirectTo: '/consulta' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
